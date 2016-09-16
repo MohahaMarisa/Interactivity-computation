419d7e3018446bdf18b79b18a459de487818c992
marisa <contactmarisalu@gmail.com>
2016-09-16 04:07:35 -0400

    finally lerpng

--- Lumar_CircleGradient/Lumar-CircleChangingOriginal.js
+++ Lumar_CircleGradient/Lumar-CircleChangingOriginal.js
@@ -1,95 +1,190 @@
-var zero = [p5.Vector Object : [315, 0, 0],p5.Vector Object : [210, 33, 0],p5.Vector Object : [140, 133, 0],p5.Vector Object : [105, 300, 0],p5.Vector Object : [105, 400, 0],p5.Vector Object : [140, 567, 0],p5.Vector Object : [210, 667, 0],p5.Vector Object : [315, 700, 0],p5.Vector Object : [385, 700, 0],p5.Vector Object : [490, 667, 0],p5.Vector Object : [560, 567, 0],p5.Vector Object : [595, 400, 0],p5.Vector Object : [595, 300, 0],p5.Vector Object : [560, 133, 0],p5.Vector Object : [490, 33, 0],p5.Vector Object : [385, 0, 0],p5.Vector Object : [315, 0, 0]];
-var one = [p5.Vector Object : [210, 133, 0],p5.Vector Object : [280, 100, 0],p5.Vector Object : [385, 0, 0],p5.Vector Object : [385, 700, 0]];
-var two = [p5.Vector Object : [140, 167, 0],p5.Vector Object : [140, 133, 0],p5.Vector Object : [175, 67, 0],p5.Vector Object : [210, 33, 0],p5.Vector Object : [280, 0, 0],p5.Vector Object : [420, 0, 0],p5.Vector Object : [490, 33, 0],p5.Vector Object : [525, 67, 0],p5.Vector Object : [560, 133, 0],p5.Vector Object : [560, 200, 0],p5.Vector Object : [525, 267, 0],p5.Vector Object : [455, 367, 0],p5.Vector Object : [105, 700, 0],p5.Vector Object : [595, 700, 0]];
-var three = [p5.Vector Object : [175, 0, 0],p5.Vector Object : [560, 0, 0],p5.Vector Object : [350, 267, 0],p5.Vector Object : [455, 267, 0],p5.Vector Object : [525, 300, 0],p5.Vector Object : [560, 333, 0],p5.Vector Object : [595, 433, 0],p5.Vector Object : [595, 500, 0],p5.Vector Object : [560, 600, 0],p5.Vector Object : [490, 667, 0],p5.Vector Object : [385, 700, 0],p5.Vector Object : [280, 700, 0],p5.Vector Object : [175, 667, 0],p5.Vector Object : [140, 633, 0],p5.Vector Object : [105, 567, 0]];
-var four = [p5.Vector Object : [455, 0, 0],p5.Vector Object : [105, 467, 0],p5.Vector Object : [630, 467, 0],p5.Vector Object : [0, 0, 0],p5.Vector Object : [455, 0, 0],p5.Vector Object : [455, 700, 0]];
-var five = [p5.Vector Object : [525, 0, 0],p5.Vector Object : [175, 0, 0],p5.Vector Object : [140, 300, 0],p5.Vector Object : [175, 267, 0],p5.Vector Object : [280, 233, 0],p5.Vector Object : [385, 233, 0],p5.Vector Object : [490, 267, 0],p5.Vector Object : [560, 333, 0],p5.Vector Object : [595, 433, 0],p5.Vector Object : [595, 500, 0],p5.Vector Object : [560, 600, 0],p5.Vector Object : [490, 667, 0],p5.Vector Object : [385, 700, 0],p5.Vector Object : [280, 700, 0],p5.Vector Object : [175, 667, 0],p5.Vector Object : [140, 633, 0],p5.Vector Object : [105, 567, 0]];
-var six = [p5.Vector Object : [560, 100, 0],p5.Vector Object : [525, 33, 0],p5.Vector Object : [420, 0, 0],p5.Vector Object : [350, 0, 0],p5.Vector Object : [245, 33, 0],p5.Vector Object : [175, 133, 0],p5.Vector Object : [140, 300, 0],p5.Vector Object : [140, 467, 0],p5.Vector Object : [175, 600, 0],p5.Vector Object : [245, 667, 0],p5.Vector Object : [350, 700, 0],p5.Vector Object : [385, 700, 0],p5.Vector Object : [490, 667, 0],p5.Vector Object : [560, 600, 0],p5.Vector Object : [595, 500, 0],p5.Vector Object : [595, 467, 0],p5.Vector Object : [560, 367, 0],p5.Vector Object : [490, 300, 0],p5.Vector Object : [385, 267, 0],p5.Vector Object : [350, 267, 0],p5.Vector Object : [245, 300, 0],p5.Vector Object : [175, 367, 0],p5.Vector Object : [140, 467, 0]];
-var seven = [p5.Vector Object : [595, 0, 0],p5.Vector Object : [245, 700, 0],p5.Vector Object : [0, 0, 0],p5.Vector Object : [105, 0, 0],p5.Vector Object : [595, 0, 0]];
-var eight = [p5.Vector Object : [280, 0, 0],p5.Vector Object : [175, 33, 0],p5.Vector Object : [140, 100, 0],p5.Vector Object : [140, 167, 0],p5.Vector Object : [175, 233, 0],p5.Vector Object : [245, 267, 0],p5.Vector Object : [385, 300, 0],p5.Vector Object : [490, 333, 0],p5.Vector Object : [560, 400, 0],p5.Vector Object : [595, 467, 0],p5.Vector Object : [595, 567, 0],p5.Vector Object : [560, 633, 0],p5.Vector Object : [525, 667, 0],p5.Vector Object : [420, 700, 0],p5.Vector Object : [280, 700, 0],p5.Vector Object : [175, 667, 0],p5.Vector Object : [140, 633, 0],p5.Vector Object : [105, 567, 0],p5.Vector Object : [105, 467, 0],p5.Vector Object : [140, 400, 0],p5.Vector Object : [210, 333, 0],p5.Vector Object : [315, 300, 0],p5.Vector Object : [455, 267, 0],p5.Vector Object : [525, 233, 0],p5.Vector Object : [560, 167, 0],p5.Vector Object : [560, 100, 0],p5.Vector Object : [525, 33, 0],p5.Vector Object : [420, 0, 0],p5.Vector Object : [280, 0, 0]];
-var nine = [p5.Vector Object : [560, 233, 0],p5.Vector Object : [525, 333, 0],p5.Vector Object : [455, 400, 0],p5.Vector Object : [350, 433, 0],p5.Vector Object : [315, 433, 0],p5.Vector Object : [210, 400, 0],p5.Vector Object : [140, 333, 0],p5.Vector Object : [105, 233, 0],p5.Vector Object : [105, 200, 0],p5.Vector Object : [140, 100, 0],p5.Vector Object : [210, 33, 0],p5.Vector Object : [315, 0, 0],p5.Vector Object : [350, 0, 0],p5.Vector Object : [455, 33, 0],p5.Vector Object : [525, 100, 0],p5.Vector Object : [560, 233, 0],p5.Vector Object : [560, 400, 0],p5.Vector Object : [525, 567, 0],p5.Vector Object : [455, 667, 0],p5.Vector Object : [350, 700, 0],p5.Vector Object : [280, 700, 0],p5.Vector Object : [175, 667, 0],p5.Vector Object : [140, 600, 0]];
+var zero; 
+var one;      
+var two;      
+var three;      
+var four;      
+var five;      
+var six;      
+var seven;      
+var eight;      
+var nine;      
+var allday;
 
 var t=0;
-//var light = [255,255,255,255,255,255,255,255,255,255,255,255
-              //150,150,150,150,150,150,150,150,150,150];
-//var shadow = [100,100,100,100,100,100,100,100,100,100,100,100];
-var H;
-var M;
+var reps = 0;
+var repstwo = 0;
+var repEnd;
+var repEndtwo;
+var lerpNum = 0;
+var numPath;
+var numPathtwo;
+
+//var hour();
+var hr;
+var min;
+var hourcircle;
+var hourtwo;
+var light;
 function setup() {
   createCanvas(700,700);
   background(230,230,250);
+  hourcircle = new HourCircle;
+  hourtwo = new HourCircle;
+  zero = [ createVector(315, 0, 0), createVector(210, 33, 0), createVector(140, 133, 0), createVector(105, 300, 0), createVector(105, 400, 0), createVector(140, 567, 0), createVector(210, 667, 0), createVector(315, 700, 0), createVector(385, 700, 0), createVector(490, 667, 0), createVector(560, 567, 0), createVector(595, 400, 0), createVector(595, 300, 0), createVector(560, 133, 0), createVector(490, 33, 0), createVector(385, 0, 0), createVector(315, 0, 0)];
+  one = [ createVector(210, 133, 0), createVector(280, 100, 0), createVector(385, 0, 0), createVector(385, 700, 0)];
+  two = [ createVector(140, 167, 0), createVector(140, 133, 0), createVector(175, 67, 0), createVector(210, 33, 0), createVector(280, 0, 0), createVector(420, 0, 0), createVector(490, 33, 0), createVector(525, 67, 0), createVector(560, 133, 0), createVector(560, 200, 0), createVector(525, 267, 0), createVector(455, 367, 0), createVector(105, 700, 0), createVector(595, 700, 0)];
+  three = [ createVector(175, 0, 0), createVector(560, 0, 0), createVector(350, 267, 0), createVector(455, 267, 0), createVector(525, 300, 0), createVector(560, 333, 0), createVector(595, 433, 0), createVector(595, 500, 0), createVector(560, 600, 0), createVector(490, 667, 0), createVector(385, 700, 0), createVector(280, 700, 0), createVector(175, 667, 0), createVector(140, 633, 0), createVector(105, 567, 0)];
+  four = [ createVector(455, 0, 0), createVector(105, 467, 0), createVector(630, 467, 0), createVector(0, 0, 0), createVector(455, 0, 0), createVector(455, 700, 0)];
+  five = [ createVector(525, 0, 0), createVector(175, 0, 0), createVector(140, 300, 0), createVector(175, 267, 0), createVector(280, 233, 0), createVector(385, 233, 0), createVector(490, 267, 0), createVector(560, 333, 0), createVector(595, 433, 0), createVector(595, 500, 0), createVector(560, 600, 0), createVector(490, 667, 0), createVector(385, 700, 0), createVector(280, 700, 0), createVector(175, 667, 0), createVector(140, 633, 0), createVector(105, 567, 0)];
+  six = [ createVector(560, 100, 0), createVector(525, 33, 0), createVector(420, 0, 0), createVector(350, 0, 0), createVector(245, 33, 0), createVector(175, 133, 0), createVector(140, 300, 0), createVector(140, 467, 0), createVector(175, 600, 0), createVector(245, 667, 0), createVector(350, 700, 0), createVector(385, 700, 0), createVector(490, 667, 0), createVector(560, 600, 0), createVector(595, 500, 0), createVector(595, 467, 0), createVector(560, 367, 0), createVector(490, 300, 0), createVector(385, 267, 0), createVector(350, 267, 0), createVector(245, 300, 0), createVector(175, 367, 0), createVector(140, 467, 0)];
+  seven = [ createVector(595, 0, 0), createVector(245, 700, 0), createVector(0, 0, 0), createVector(105, 0, 0), createVector(595, 0, 0)];
+  eight = [ createVector(280, 0, 0), createVector(175, 33, 0), createVector(140, 100, 0), createVector(140, 167, 0), createVector(175, 233, 0), createVector(245, 267, 0), createVector(385, 300, 0), createVector(490, 333, 0), createVector(560, 400, 0), createVector(595, 467, 0), createVector(595, 567, 0), createVector(560, 633, 0), createVector(525, 667, 0), createVector(420, 700, 0), createVector(280, 700, 0), createVector(175, 667, 0), createVector(140, 633, 0), createVector(105, 567, 0), createVector(105, 467, 0), createVector(140, 400, 0), createVector(210, 333, 0), createVector(315, 300, 0), createVector(455, 267, 0), createVector(525, 233, 0), createVector(560, 167, 0), createVector(560, 100, 0), createVector(525, 33, 0), createVector(420, 0, 0), createVector(280, 0, 0)];
+  nine = [ createVector(560, 233, 0), createVector(525, 333, 0), createVector(455, 400, 0), createVector(350, 433, 0), createVector(315, 433, 0), createVector(210, 400, 0), createVector(140, 333, 0), createVector(105, 233, 0), createVector(105, 200, 0), createVector(140, 100, 0), createVector(210, 33, 0), createVector(315, 0, 0), createVector(350, 0, 0), createVector(455, 33, 0), createVector(525, 100, 0), createVector(560, 233, 0), createVector(560, 400, 0), createVector(525, 567, 0), createVector(455, 667, 0), createVector(350, 700, 0), createVector(280, 700, 0), createVector(175, 667, 0), createVector(140, 600, 0)];
+
+  allday = [zero,one,two,three,four,five,six,seven,eight,nine];
+
+  shadow = [/*12*/createVector(0,0,0), /*1*/createVector(0,0,0), /*2*/createVector(0,0,0), /*3*/createVector(60,56,32), /*4*/createVector(83,82,62), /*5*/createVector(119,122,106),/*6am*/createVector(160,166,142), /*7am*/createVector(138,165,140),/*8am*/createVector(121,150,135), /*9am*/createVector(110,136,144),  /*10am*/createVector(96,103,132), /*11am*/createVector(138,165,140),  /*12pm*/createVector(95,81,140), /*1pm*/createVector(79,77,154),/*2pm*/createVector(79,104,176), /*3pm*/createVector(66,113,172), /*4pm*/createVector(59,142,166), /*5pm*/createVector(41,135,136), /*6pm*/createVector(32,121,103),/*7pm*/createVector(15,76,32), /*8pm*/createVector(45,49,7), /*9pm*/createVector(47,31,5), /*10*/createVector(0,0,0), /*11*/createVector(0,0,0) ]; 
+  light = [/*12*/createVector(120,117,174), /*1*/createVector(131,138,160), /*2*/createVector(116,121,133), /*3*/createVector(94,96,100), /*4*/createVector(132,136,142), /*5*/createVector(178,157,177),/*6am*/createVector(216,187,201), /*7am*/createVector(230,195,201),/*8am*/createVector(238,210,199), /*9am*/createVector(241,219,200),  /*10am*/createVector(250,231,204), /*11am*/createVector(255,245,206),  /*12pm*/createVector(255,252,216), /*1pm*/createVector(255,247,205),/*2pm*/createVector(255,238,188), /*3pm*/createVector(255,220,164), /*4pm*/createVector(255,210,157), /*5pm*/createVector(255,203,157), /*6pm*/createVector(255,133,73),/*7pm*/createVector(255,99,73), /*8pm*/createVector(224,79,130), /*9pm*/createVector(171,89,145), /*10*/createVector(145,89,171), /*11*/createVector(108,89,171)]; 
 }
 
 function draw() {
   // Fetch the current time
-  H = hour();
-  M = minute();
-    if(mouseIsPressed){
+  //hour() = hour();
+  min = minute();
+  var lightnowr= light[hour()].x;
+  var lightnowg= light[hour()].y;
+  var lightnowb= light[hour()].z;
+  var darknowr= shadow[hour()].x;
+  var darknowg= shadow[hour()].y;
+  var darknowb= shadow[hour()].z;
+  hourcircle.draw(t, lightnowr, lightnowg, lightnowb, darknowr, darknowg, darknowb);
+  hr = hour();
+  if((!(hr > 9))||(hr>12)){
+    numPath = allday[(hr%12)];
+    var amountOfvertice = numPath.length;
+    reps=reps%(amountOfvertice-1);
+    repEnd=(reps+1)%(amountOfvertice);//rep = which vertice to go through
+    var testingx = numPath[repEnd].x;
+    var testingy = numPath[repEnd].y;
+    var sum = testingx+testingy;
+    if (sum <= 0){
+      reps+=2;//this is to lift up the circle gradient when necesary if there's a break in path
     }
-    else{
-      
-      push();
-        translate(mouseX,mouseY);
-        rotate(t*10);
-         stroke(startr+incrementr*i, startg+incrementg*i,startb+incrementb*i);
-         line(-radius+i,-lengthOfLine,-radius+i,lengthOfLine);
-        
-      pop();
+    hourcircle.move(numPath[reps],numPath[repEnd],lerpNum);
+    lerpNum+=1/100;
+    if(lerpNum > 1){
+      lerpNum=0;
+      reps++;
     }
   }
-  t+=1/1000;
+  else if((hr = 11) || (hr = 23)){
+    numPath = allday[1];
+    var amountOfvertice = numPath.length;
+    reps=reps%(amountOfvertice-1);
+    repEnd=(reps+1)%(amountOfvertice);//rep = which vertice to go through
+    var testingx = numPath[repEnd].x;
+    var testingy = numPath[repEnd].y;
+    var sum = testingx+testingy;
+    if (sum <= 0){
+      reps+=2;//this is to lift up the circle gradient when necesary if there's a break in path
+    }
+    hourcircle.move(numPath[reps],numPath[repEnd],lerpNum);
+    hourtwo.move(numPath[reps],numPath[repEnd],lerpNum);
+    lerpNum+=1/100;
+    if(lerpNum > 1){
+      lerpNum=0;
+      reps++;
+    }
+  }
+  else if((hr=12)||(hr=0)){
+    numPath = allday[1];
+    numPathtwo = allday[2];
+    var amountOfvertice = numPath.length;
+    var amountOfverticetwo = numPathtwo.length;
+    reps=reps%(amountOfvertice-1);
+    repstwo=repstwo%(amountOfverticetwo-1);
+    repEnd=(reps+1)%(amountOfvertice);//rep = which vertice to go through
+    repEndtwo=(reps+1)%(amountOfverticetwo);
+    var testingx = numPath[repEnd].x;
+    var testingy = numPath[repEnd].y;
+    var sum = testingx+testingy;
+    if (sum <= 0){
+      reps+=2;//this is to lift up the circle gradient when necesary if there's a break in path
+    }
+    hourcircle.move(numPath[reps],numPath[repEnd],lerpNum);
+    hourtwo.move(numPathtwo[repstwo],numPathtwo[repEndtwo],lerpNum);
+    lerpNum+=1/100;
+    if(lerpNum > 1){
+      lerpNum=0;
+      reps++;
+    }
+  }
+  t+=Math.PI*1/900000;
 }
 function HourCircle(){
   this.radius = 90;
-  this.diameter = radius*2; 
-  /*
-  this.lightr = 245;
-  this.lightg = 230;
-  this.lightb = 170;
-  this.shadowr = 235;
-  this.shadowg = 100;
-  this.shadowb = 120;*/
+  this.diameter = this.radius*2; 
   this.x = 0.1 * width;
   this.y = 10 * height;
-  this.changeColor = function(){
-  }
-  this.draw = function(){
-    this.lightr = 55*noise(t+6)+200;
-    this.lightg = 55*noise(t+20)+200;
-    this.lightb = 55*noise(t+1)+200;
-    this.shadowr = 210-210*noise(t);
-    this.shadowg = 210-210*noise(t+9);
-    this.shadowb = 210-210*noise(t+4);
-    for (var i = 1; i <= 180; i++){
-    this.incrementr = (r-startr)/180;
-    this.incrementg = (g-startg)/180;
-    this.incrementb = (b-startb)/180;
-    this.angle = (i/180)*Math.PI;
+  this.draw = function(t,lightr,lightg,lightb, shadowr,shadowg, shadowb){
+    for (var i = 1; i <= this.diameter; i++){
+      this.incrementr = (shadowr-lightr)/180;
+      this.incrementg = (shadowg-lightg)/180;
+      this.incrementb = (shadowb-lightb)/180;
     //fullcircle
-    this.lengthOfLine = Math.abs(radius*(Math.sin(Math.acos((90-i)/radius))));
+      this.lengthOfLine = Math.abs(this.radius*(Math.sin(Math.acos((90-i)/this.radius))));
     //halfcircle
-    //this.lengthOfLine = Math.abs(radius*(Math.sin(Math.acos(i/radius))));
+    //this.lengthOfLine = Math.abs(this.radius*(Math.sin(Math.acos(i/this.radius))));
     //diamond
-    //this.lengthOfLine = Math.abs(radius*(Math.sin(angle)));
-    push();
-        translate(mouseX,mouseY);
-        rotate(t*1000);
-         stroke(startr+incrementr*i, startg+incrementg*i,startb+incrementb*i);
-         line(-radius+i,-lengthOfLine,-radius+i,lengthOfLine);
-        
+    //this.angle = (i/180)*Math.PI;
+    //this.lengthOfLine = Math.abs(this.radius*(Math.sin(this.angle)));
+      this.angle=(hour()+18)*-1*Math.PI/24;
+      push();
+        translate(this.x,this.y);
+        rotate(this.angle);
+        stroke(lightr+this.incrementr*i, lightg+this.incrementg*i,lightb+this.incrementb*i);
+        line(-this.radius+i,-this.lengthOfLine,-this.radius+i,this.lengthOfLine);   
       pop();
-
+    }
   }
-  this.move = function(){
-
+  this.move = function(startv, endv, lerpNum){
+    //println(endv);
+    this.x = lerp(startv.x, endv.x, lerpNum);
+    this.y = lerp(startv.y, endv.y, lerpNum);
   }
 
 }
+
 function MinuteCircle(){
-  this.changeColor = function(){
-    
+  this.radius = 45;
+  this.diameter = this.radius*2; 
+  this.x = -100;
+  this.y = -100;
+  this.draw = function(t,lightr,lightg,lightb, shadowr,shadowg, shadowb){
+    for (var i = 1; i <= this.diameter; i++){
+      this.incrementr = (shadowr-lightr)/180;
+      this.incrementg = (shadowg-lightg)/180;
+      this.incrementb = (shadowb-lightb)/180;
+    //fullcircle
+      this.lengthOfLine = Math.abs(this.radius*(Math.sin(Math.acos((90-i)/this.radius))));
+    //halfcircle
+    //this.lengthOfLine = Math.abs(this.radius*(Math.sin(Math.acos(i/this.radius))));
+    //diamond
+    this.angle = (i/180)*Math.PI;
+    this.lengthOfLine = Math.abs(this.radius*(Math.sin(this.angle)));
+      this.rotation=(hour()+18)*-1*Math.PI/24;//REFLECTS POSITION OF SUN IN SKY
+      push();
+        translate(this.x,this.y);
+        rotate(this.angle);
+        stroke(lightr+this.incrementr*i, lightg+this.incrementg*i,lightb+this.incrementb*i);
+        line(-this.radius+i,-this.lengthOfLine,-this.radius+i,this.lengthOfLine);   
+      pop();
+    }
+  }
+  this.move = function(startv, endv, lerpNum){
+    //println(endv);
+    this.x = lerp(startv.x, endv.x, lerpNum);
+    this.y = lerp(startv.y, endv.y, lerpNum);
   }
 }
-function changing(){
 
-}
